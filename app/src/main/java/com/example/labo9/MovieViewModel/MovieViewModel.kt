f85c761e53ae1142c48138c207e0687adcbc61fc
juliof9 <00081817@uca.edu.sv>
2019-06-07 08:12:38 -0600

    Agg parte del ViewModel

--- app/src/main/java/com/example/labo9/MovieViewModel/MovieViewModel.kt
+++ app/src/main/java/com/example/labo9/MovieViewModel/MovieViewModel.kt
@@ -1,4 +1,19 @@
 package com.example.labo9.MovieViewModel
 
-class MovieViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import com.example.labo9.Entity.Movie
+import com.example.labo9.MovieDatabase.ModieDB
+import com.example.labo9.MovieRepository.MovieRespository
+
+class MovieViewModel (app: Application) : AndroidViewModel(app) {
+    private val repository : MovieRespository
+    val allMovies : LiveData<List<Movie>>
+
+    init {
+        val movieDao = ModieDB.getInstance(app).movieDao()
+        repository = MovieRespository(movieDao)
+        movieDao = repository.allMovies
+    }
 }
